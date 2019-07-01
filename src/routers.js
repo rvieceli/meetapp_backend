8dbe6aea729cfbaa5116a2b0dfc9cd294608699e
Rafael Ricardo Vieceli <rafaelricardo3@gmail.com>
2019-07-01 00:18:51 +0000

    Implementação de upload de arquivo e listagem

--- src/routers.js
+++ src/routers.js
@@ -1,11 +1,16 @@
 import { Router } from 'express';
+import multer from 'multer';
+import multerConfig from './config/multer';
+
 import UserController from './app/controllers/UserController';
 import SessionController from './app/controllers/SessionController';
 import PasswordController from './app/controllers/PasswordController';
+import FileController from './app/controllers/FileController';
 
 import authMiddleware from './app/middlewares/auth';
 
 const routes = new Router();
+const upload = multer(multerConfig);
 
 routes.post('/users', UserController.store);
 routes.post('/sessions', SessionController.store);
@@ -14,5 +19,8 @@ routes.use(authMiddleware);
 
 routes.put('/users', UserController.update);
 routes.put('/users/password', PasswordController.update);
+
+routes.get('/files', FileController.index);
+routes.post('/files', upload.single('file'), FileController.store);
 
 export default routes;
